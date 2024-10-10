3dfe9f599f57302010acceaed5ee2fc9b1a8e2ae
cloud-hot <wei.a.yang@gmail.com>
2024-10-10 05:41:04 -0700

    use static site instead of SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/serverless';
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
   site: "https://svgcs.com",
   integrations: [mdx(), sitemap(), tailwind()],
 });
